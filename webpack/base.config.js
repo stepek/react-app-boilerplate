6b3b651999b46893738ceeadb4e3475a456e461a
Kamil Stepczuk <k.stepczuk@hotmail.com>
2016-03-22 08:19:29 +0100

    Commit backbone of application.

---
+++ webpack/base.config.js
@@ -0,0 +1,66 @@
+var port = 8081;
+var hostname = 'localhost';
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var output = path.join(__dirname, '../dist');
+
+module.exports = {
+  context: path.resolve(__dirname, '..'),
+  entry: [
+    './app/js/app'
+  ],
+  output: {
+    path: output,
+    filename: 'bundle.js',
+    sourceMapFilename: '[file].map'
+  },
+  module: {
+    loaders: [
+      {
+        test: /.js.*?$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        },
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.(woff|svg|ttf|eot)([\?]?.*)$/,
+        loader: 'file-loader?name=[name].[ext]'
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+          'file?hash=sha512&digest=hex&name=[hash].[ext]',
+          'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!autoprefixer-loader')
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader?sourceMap!sass?sourceMap')
+      },
+      {
+        test: /\.less$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader?sourceMap!less?sourceMap')
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './app/index.html',
+      inject: 'body'
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new ExtractTextPlugin('styles.css')
+  ],
+  devServer: {
+    host: hostname,
+    port: port
+  }
+};
